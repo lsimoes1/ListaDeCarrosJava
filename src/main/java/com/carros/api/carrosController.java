2893774f7824d9d5252a83b619be0ca6856533bc
Luan <luan.simoes1@gmail.com>
2020-05-02 11:23:14 -0300

    Adicionado o método getCarroById()

--- src/main/java/com/carros/api/carrosController.java
+++ src/main/java/com/carros/api/carrosController.java
@@ -1,7 +1,10 @@
 package com.carros.api;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +22,9 @@ public class carrosController {
 	public Iterable<Carro> get() {
 		return service.getCarros();
 	}
-
+	
+	@GetMapping("/{id}")
+	public Optional<Carro> get(@PathVariable("id") Long id) {
+		return service.getCarroById(id);
+	}
 }
--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -2,6 +2,7 @@ package com.carros.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,10 @@ public class CarroService {
 
 		return rep.findAll();
 
+	}
+	
+	public Optional<Carro> getCarroById(Long id){
+		return rep.findById(id);
 	}
 
 	public List<Carro> getCarrosFake() {
